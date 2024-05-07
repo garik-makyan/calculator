77ca3d07a6d717efa669429636034f7e3f060bdc
garik-makyan <111341966+garik-makyan@users.noreply.github.com>
2024-05-07 15:36:30 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+<script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '40px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '40px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let  = x.document.getElementById('demo');
+        x.style.fontSize = '40px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '25px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '25px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '25px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '25px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById("demo");
+        x.style.fontSize = '25px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '25px';
+        x.style.color = 'red';
+      }
+    </script>
+    <script>
+      function myFunction() {
+        let = x.document.getElementById('demo');
+        x.style.fontSize = '25px';
+        x.style.color = 'red';
+      }
+    </script>
